e69d01acb2b073c81a1491a752082b13136f7358
Marwan <matrix105.mt@gmail.com>
2021-06-06 21:15:07 +0100

    icon fixes

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { FaGithub, FaAndroid,FaApple,FaGoogle } from 'react-icons/fa';
+import { FaGithub, FaNode, FaReact, FaGoogle, FaPython } from "react-icons/fa";
 
 const Hero = () => {
-	return (
-		<div className='flex flex-wrap items-center justify-center p-3 m-auto md:px-10 md:py-5'>
+  return (
+    <div className="flex flex-wrap items-center justify-center p-3 m-auto md:px-10 md:py-5">
+      <div className="flex flex-col items-start p-4 leading-snug lg:w-1/2 lg:p-12 space-y-5">
+        <span className="inline-block w-auto px-2 py-1 text-xs text-center rounded-sm text-gray1 bg-green-lighter">
+          Software Engineer
+        </span>
+        <h2 className="text-3xl">
+          Hello, I’m Marwan Tourky, a Software Engineer and Fullstack Developer
+        </h2>
+        <p className="text-sm text-gray2">I enjoy building Web Applications.</p>
+        <a
+          className="px-4 py-2 mt-5 border-2 rounded-sm hover:bg-green-lighter hover:text-gray1 text-green-lighter border-green-lighter"
+          href="mailto:contact@marwant.me"
+        >
+          Say hi!
+        </a>
+      </div>
 
-			<div className='flex flex-col items-start p-4 leading-snug lg:w-1/2 lg:p-12 space-y-5'>
-				<span className='inline-block w-auto px-2 py-1 text-xs text-center rounded-sm text-gray1 bg-green-lighter'>Software Engineer</span>
-				<h2 className='text-3xl'>Hello, I’m Marwan Tourky, a Software Engineer and Fullstack Developer</h2>
-				<p className='text-sm text-gray2'>I enjoy building Web Applications.</p>
-				<a className='px-4 py-2 mt-5 border-2 rounded-sm hover:bg-green-lighter hover:text-gray1 text-green-lighter border-green-lighter' href='mailto:contact@marwant.me'>Say hi!</a>
-			</div>
-
-			<div className='flex items-center justify-end px-2 py-4 lg:-mt-10 lg:w-1/2 space-x-8 xl:p-12'>
-				<FaAndroid className='text-6xl opacity-25 xm:text-8xl' />
-				<FaApple className='text-6xl opacity-25 xm:text-8xl' />
-				<FaGithub className='text-6xl opacity-25 xm:text-8xl' />
-				<FaGoogle className='text-6xl opacity-25 xm:text-8xl' />
-
-			</div>
-
-		</div>
-	);
+      <div className="flex items-center justify-end px-2 py-4 lg:-mt-10 lg:w-1/2 space-x-8 xl:p-12">
+        <FaNode className="text-6xl opacity-25 xm:text-8xl" />
+        <FaReact className="text-6xl opacity-25 xm:text-8xl" />
+        <FaGithub className="text-6xl opacity-25 xm:text-8xl" />
+        <FaPython className="text-6xl opacity-25 xm:text-8xl" />
+      </div>
+    </div>
+  );
 };
 
 // button 'say hi'
--- components/Introduce/Resume.jsx
+++ components/Introduce/Resume.jsx
@@ -8,7 +8,7 @@ const Resume = () => {
         Hi! My name is <span className="border-b-1">Marwan Tourky</span>
       </h2>
       <p className="my-5 text-justify text-xm text-gray2">
-        I'm a software engineer who is passionate about building android or iOS
+        I'm a software engineer who is passionate about building web and mobile
         applications with <span className="text-green-lighter">React JS</span>{" "}
         and <span className="text-green-lighter">Python</span>. Creating
         technology to elevate people, and building community. Some technologies
