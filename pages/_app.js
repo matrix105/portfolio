1942c7bf2becaa82ee188241f2701c885eff90c3
Marwan <matrix105.mt@gmail.com>
2023-12-14 20:59:21 +0000

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import '../css/tailwind.css';
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+import "../css/tailwind.css";
 
 const MyApp = ({ Component, pageProps }) => {
-    return (
-			<React.Fragment>
-				<style jsx global>{`
-					@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&display=swap');
-				`}</style>
-				<Component {...pageProps} />
-			</React.Fragment>
-		);
+  return (
+    <React.Fragment>
+      <style jsx global>{`
+        @import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600;700&display=swap");
+      `}</style>
+      <Component {...pageProps} />
+      <Analytics />
+    </React.Fragment>
+  );
 };
 
 export default MyApp;
